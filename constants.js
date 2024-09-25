cab14f30c774e08213740da18e26505e1b1415cf
rajashekarmanda <rajashekarmanda3729@gmail.com>
2024-09-25 10:10:47 +0530

    Added constants and Grievance module testing

---
+++ constants.js
@@ -0,0 +1,9 @@
+export const SDK_ENVIRONMENT = {
+  stagSandbox: "https://card-webview.sb.stag.card91.in",
+  stagLive: "https://card-webview.stag.card91.in",
+  sandbox: "https://card-webview-sandbox.card91.io",
+  live: "https://card-webview.card91.io",
+  dev: "https://card-webview.dev.card91.in",
+  qa: "https://card-webview.qa.card91.in",
+  local: "http://localhost:3000",
+};
