b313a57d2dc4175186aa9b0bbb6b698c36df7448
rajashekarmanda <rajashekarmanda3729@gmail.com>
2025-03-25 10:10:26 +0530

    ruCard webview added

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ export const SDK_ENVIRONMENT = {
   stagLive: "https://card-webview.stag.card91.in",
   sandbox: "https://card-webview-sandbox.card91.io",
   live: "https://card-webview.card91.io",
-  dev: "https://card-webview.dev.card91.in",
+  dev: "https://card-webview.develop.card91.in",
   qa: "https://card-webview.qual.card91.in",
   local: "http://localhost:3000",
 };
